1811e555148c444763c0976b1565e46c98ac79d8
Adam PM <adam.pm77@gmail.com>
2020-09-13 06:06:37 +0700

    repair title

--- application/views/layouts/_head.php
+++ application/views/layouts/_head.php
@@ -1,7 +1,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>AdminLTE 2 | Starter</title>
+	<title>BAIK Marketplace | CMS</title>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<link rel="icon" type="image/x-icon" href="<?= base_url(); ?>favicon.ico">
 	<link rel="stylesheet" href="<?= base_url(); ?>vendor/almasaeed2010/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
